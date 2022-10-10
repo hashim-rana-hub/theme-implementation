2151d09c836569799306088be23ebfa917843c82
hashim-rana-hub <ahashimrana@hmail.com>
2022-10-10 19:22:00 +0500

    theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box, Switch, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
+import {darkTheme, lightTheme} from "./styles/Theme"
 
 function App() {
+  const [toggle, setToggle] = useState(true)
   return (
+    <>
+    <ThemeProvider theme={toggle? {...darkTheme} : {...lightTheme} }>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch onChange={()=> setToggle(!toggle)} />
+      <Box sx={{
+        width: "200px",
+        height: "200px",
+        backgroundColor: "primary.main",
+        border: "1px solid green"
+      }}>
+      
+      </Box>
     </div>
+    </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/styles/Theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const darkTheme = createTheme({
+    palette: {
+        primary :{
+            main: "#000"
+        }
+    }
+})
+export const lightTheme = createTheme({
+    palette: {
+        primary :{
+            main: "#00ff00"
+        }
+    }
+})
